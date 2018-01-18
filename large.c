1e9b95c00ade101f1c70fb143ee6762ad913d1be
naveenbalaji <30828376+naveenbalaj@users.noreply.github.com>
2018-01-18 16:21:35 +0530

    Rename largest.c to large.c

---
+++ large.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int p,q,r;
+printf("enter the numbers");
+scanf("%d %d %d",&p,&q,&c);
+if(p>q)
+{
+if(p>r)
+{
+printf("%d is the largest number",p);
+}
+else(q>r)
+{
+printf("%d is largest number",q);
+}
+}
+else
+print("%d is largest number",r);
+}
