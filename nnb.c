ee7a31a6c506674cbe11e3e7e8f08c8ac61096c8
naveenbalaji <30828376+naveenbalaj@users.noreply.github.com>
2018-01-18 15:55:43 +0530

    Create nnb.c

---
+++ nnb.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int num;
+printf("Enter a number");
+scanf("%d", &num);
+if (num 0)
+printf("%d is a positive number", num);
+else if (num < 0)
+printf("%d is a negative number", num);
+else
+printf("0 is neither positive nor negative");
+}
